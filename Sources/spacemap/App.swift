dcdd47d808adc7288624b91ed01353df471111cb
Jeff Sheffield <jeff.sheffield@gmail.com>
2026-05-13 10:47:21 -0500

    added a menubar

--- Sources/spacemap/App.swift
+++ Sources/spacemap/App.swift
@@ -5,9 +5,11 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     private var hotkey: HotkeyMonitor?
     private var socketListener: SocketListener?
     private let socketPath = "/tmp/spacemap_\(NSUserName()).socket"
+    private var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.prohibited)
+        setupMenubar()
         // Delay slightly so TCC/LaunchServices finishes registering the app
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.startHotkey()
@@ -21,6 +23,28 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ notification: Notification) {
         YabaiClient.removeSignals()
         socketListener?.stop()
+    }
+
+    private func setupMenubar() {
+        let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = item.button {
+            button.image = NSImage(systemSymbolName: "square.grid.3x3", accessibilityDescription: "spacemap")
+        }
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Show/Hide Map", action: #selector(toggleHUD), keyEquivalent: ""))
+        menu.addItem(.separator())
+        menu.addItem(NSMenuItem(title: "Open Accessibility Permissions", action: #selector(openAccessibility), keyEquivalent: ""))
+        menu.addItem(.separator())
+        menu.addItem(NSMenuItem(title: "Quit spacemap", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        item.menu = menu
+        statusItem = item
+    }
+
+    @objc private func toggleHUD() { hud.toggle() }
+
+    @objc private func openAccessibility() {
+        let url = URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!
+        NSWorkspace.shared.open(url)
     }
 
     private func startHotkey() {
